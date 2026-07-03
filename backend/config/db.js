f87fc7b8f8580dade4341623c533222d0aea8d45
Malick Arslan <malikarslan4999@gmail.com>
2026-07-03 14:00:57 +0500

    backend setup

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    throw new Error("MONGODB_URI is not defined in environment variables");
+  }
+
+  try {
+    await mongoose.connect(uri);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
+require ("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
+
+
 
 const app = express();
 const PORT = 5000;
 
+connectDB();
+
 app.get("/", (req, res) => {
     res.send("Backend is running!");
 });
-
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
